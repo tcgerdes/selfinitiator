02c812c967bbb14676389c2be75c9a29e3473c2a
tcgerdes <41541944+tcgerdes@users.noreply.github.com>
2018-11-03 12:37:45 +0100

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 export default () => (
-  <div style={{ color: `purple` }}>
+  <div style={{ color: `black` }}>
     <h1>Hello!</h1>
     <p>This is my page of Tobias.</p>
   </div>
